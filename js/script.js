0e63fbe33b20e2cf6d4dc3834e69f0e10222e769
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-13 17:19:29 +1200

    Plan function with pseudocode

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// CREATE function getComputerChoice
+  // SET choice to null
+  // GET random number from 0-2 for rock paper scissors
+  // IF random number is 0 THEN
+    // SET choice to rock
+  // ENDIF
+  // ELSE IF random number is 1 THEN
+    // SET choice to paper
+  // ENDIF
+  // ELSE IF random number is 2 THEN
+    // SET choice to scissors
+  // ENDIF
+  // ELSE
+    // ERROR
+  // ENDELSE
