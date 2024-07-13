af2838226408b22100688118eaadb2632d2ce3f8
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-13 17:39:40 +1200

    Return random generated option between rock paper and scissors

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 // CREATE function getComputerChoice
-  // SET choice to null
+function getComputerChoice() {
+  // SET choice to empty string
+  let choice = "";
   // GET random number from 0-2 for rock paper scissors
+  let randomNum = Math.floor(Math.random() * 3);
+  console.log(randomNum);
   // IF random number is 0 THEN
+  if (randomNum === 0) {
     // SET choice to rock
-  // ENDIF
+    choice = "rock";
+  }
   // ELSE IF random number is 1 THEN
+  else if (randomNum === 1) {
     // SET choice to paper
-  // ENDIF
+    choice = "paper";
+  }
   // ELSE IF random number is 2 THEN
+  else if (randomNum === 2) {
     // SET choice to scissors
-  // ENDIF
-  // ELSE
-    // ERROR
-  // ENDELSE
+    choice = "scissors";
+  }
+
+  return choice;
+}
+
+getComputerChoice()
