762a7aff7b03fec0bdae65fb3bfc9554e347826a
oliverhuangcode <oliverwhuang@gmail.com>
2024-07-13 18:01:07 +1200

    Plan round of rock paper scissor logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // CREATE function getComputerChoice
 function getComputerChoice() {
   // SET choice to empty string
@@ -36,3 +39,43 @@ function getHumanChoice() {
   }
   return choice;
 }
+
+// FUNC playRound(humanChoice, computerChoice)
+  // INIT outcome
+  // IF humanChoice equal to computerChoice
+    // PRINT draw
+    // RETURN
+  // ENDIF
+  // ELSE IF humanChoice is rock THEN
+    // IF computerChoice is paper THEN
+      // SET outcome to lose
+    // ENDIF
+    // IF computerChoice is scissors THEN
+      // SET outcome to win
+    // ENDIF
+  // ENDIF
+  // ELSE IF humanChoice is paper THEN
+    // IF computerChoice is rock THEN
+      // SET outcome to win
+    // ENDIF
+    // IF computerChoice is scissors THEN
+      // SET outcome to lose
+    // ENDIF
+  // ENDIF
+  // ELSE IF humanChoice is scissors
+    // IF computerChoice is rock THEN
+      // SET outcome to lose
+    // ENDIF
+    // IF computerChoice is paper THEN
+      // SET outcome to win
+    // ENDIF
+  // ENDIF
+  // IF outcome is win
+    // PRINT win message
+    // INCREMENT humanScore
+  // ENDIF
+  // ELSE IF outcome is lose
+    // PRINT lose message
+    // INCREMENT computerScore
+  // ENDIF
+  
